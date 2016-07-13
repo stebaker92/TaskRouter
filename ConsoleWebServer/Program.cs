577827be8f802241b75bdeef8b71eb3c9867dd66
Stephen Baker <stephen.baker@UK.CF247.NET>
2016-07-13 11:20:43 +0100

    inital code commit

---
+++ ConsoleWebServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWebServer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static HttpListenerResponse SendResponse(HttpListenerContext ctx)
+        {
+            HttpListenerRequest request = ctx.Request;
+            HttpListenerResponse response = ctx.Response;
+
+            String endpoint = request.RawUrl;
+
+            if (endpoint.EndsWith("assignment_callback"))
+            {
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.ContentType = "application/json";
+                response.StatusDescription = "{}";
+                return response;
+            }
+            response.StatusCode = (int)HttpStatusCode.OK;
+            return response;
+        }
+    }
+}
+
---
+++ TaskRouter/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using Twilio.TaskRouter;
+
+namespace TaskRouter.Controllers
+{
+    public class HomeController : ApiController
+    {
+        const string AccountSid = "";
+        const string AuthToken = "";
+        const string WorkspaceSid = "";
+        const string WorkflowSid = "";
+
+        static Twilio.TaskRouter.TaskRouterClient client = new Twilio.TaskRouter.TaskRouterClient(AccountSid, AuthToken);
+        public class AssignmentCallback
+        {
+            public string TaskSid { get; set; }
+            public string ReservationSid { get; set; }
+        }
+        [HttpGet]
+        [Route("home/test")]
+        public IHttpActionResult Test()
+        {
+            return Ok("Hello world");
+        }
+
+        [HttpPost]
+        [Route("home/assignment_callback")]
+        public IHttpActionResult Assignment_Callback(AssignmentCallback o)
+        {
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, new { instruction = "accept" }));
+        }
+
+        [HttpGet]
+        [Route("home/createTask")]
+        public IHttpActionResult CreateTask()
+        {
+            Task task = client.AddTask(WorkspaceSid, "{\"selected_language\":\"es\"}", WorkflowSid, null, 0);
+            if (task.RestException?.Message != null)
+            {
+                return BadRequest(task.RestException.Message);
+            }
+            return Ok($"Created task {task.Attributes + " " + task.Sid} - " + DateTime.Now);
+        }
+
+        public class ReservationCallback
+        {
+            public string TaskSid { get; set; }
+            public string ReservationSid { get; set; }
+        }
+        [HttpPost]
+        [Route("home/accept_reservation")]
+        public IHttpActionResult AcceptReservation([FromUri] ReservationCallback callback)
+        {
+            var taskSid = callback.TaskSid;
+            var reservationSid = callback.ReservationSid;
+            Reservation reservation = client.UpdateReservation(WorkspaceSid, taskSid, reservationSid, "accepted", null);
+            if(reservation.RestException?.Message != null)
+            {
+                return BadRequest(reservation.RestException.Message);
+            }
+            return Ok($"AcceptReservation: {reservation.ReservationStatus} " + DateTime.Now);
+        }
+
+    }
+}
