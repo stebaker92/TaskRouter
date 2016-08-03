122d1615343ededa57a2805f8498fb5006260203
Stephen Baker <ste.baker.92@gmail.com>
2016-08-03 14:03:28 +0100

    fixed routes

--- TaskRouter/Controllers/HomeController.cs
+++ TaskRouter/Controllers/HomeController.cs
@@ -21,21 +21,21 @@ namespace TaskRouter.Controllers
             public string ReservationSid { get; set; }
         }
         [HttpGet]
-        [Route("home/test")]
+        [Route("test")]
         public IHttpActionResult Test()
         {
             return Ok("Hello world");
         }
 
         [HttpPost]
-        [Route("home/assignment_callback")]
+        [Route("assignment_callback")]
         public IHttpActionResult Assignment_Callback(AssignmentCallback o)
         {
             return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, new { instruction = "accept" }));
         }
 
         [HttpGet]
-        [Route("home/createTask")]
+        [Route("createTask")]
         public IHttpActionResult CreateTask()
         {
             Task task = client.AddTask(WorkspaceSid, "{\"selected_language\":\"es\"}", WorkflowSid, null, 0);
@@ -52,7 +52,7 @@ namespace TaskRouter.Controllers
             public string ReservationSid { get; set; }
         }
         [HttpPost]
-        [Route("home/accept_reservation")]
+        [Route("accept_reservation")]
         public IHttpActionResult AcceptReservation([FromUri] ReservationCallback callback)
         {
             var taskSid = callback.TaskSid;
